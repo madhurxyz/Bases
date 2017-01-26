82aa4256171d814fa176f2c4638a1158fd324eff
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-25 23:08:09 -0800

    built functionality for decode() function

--- bases.py
+++ bases.py
@@ -23,6 +23,11 @@ def encode(num, base):
     length = len(str_num)
     decode_sum = 0
     for i in range(0,length):
+        int_num = int(str_num[i])
+        conversion = base**(length - i - 1)
+        decode_num = int_num*conversion
+
+    return decode_num
 
 def convert(str_num, base1, base2):
     """
